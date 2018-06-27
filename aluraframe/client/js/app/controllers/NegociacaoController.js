c77488db7ce2727193236c127bbb6a0c96992f76
AndreLucasrs <andrelrs.v@outlook.com>
2018-06-27 00:29:45 -0300

    Aula 3, estamos informando que o querySelector irá para a variável $

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,20 +1,19 @@
 class NegociacaoController{
 
-	adiciona(){
+	adiciona(event){
 		//isso cancela a atualização da pagina na hora de submeter
 		event.preventDefault();
-		alert("chamei ação no controller");
 
+		//estamos informando que o querySelector irá para a variável $, mas ainda manterá uma associação com document
+		let $ = document.querySelector.bind(document);
 		//Se declaramos as variáveis usando o let, estas ganharam um escopo de bloco	
-		let $ = document.querySelector;
-		let inputData = $('#data');
-		let inputQuantidade = $('#quantidade');
-		let inputValor = 4('#valor');
+        let inputData = $('#data');
+        let inputQuantidade =  $('#quantidade');
+        let inputValor = $('#valor');
 
-		console.log(inputData.value);
-		console.log(inputQuantidade.value);
-		console.log(inputValor.value);
+        console.log(inputData.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
 
 	}
-
 }
