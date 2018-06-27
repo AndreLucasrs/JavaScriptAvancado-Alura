b79872d53506ec52bb4810650a58bd30ab593f1f
AndreLucasrs <andrelrs.v@outlook.com>
2018-06-27 00:32:36 -0300

    Melhorando a performace, devido da antiga forma, toda hora estavamos percorrendo no DOM

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,19 +1,22 @@
 class NegociacaoController{
 
+	constructor(){
+		//Se declaramos as variáveis usando o let, estas ganharam um escopo de bloco	
+		//estamos informando que o querySelector irá para a variável $, mas ainda manterá uma associação com document
+		let $ = document.querySelector.bind(document);
+        this.inputData = $('#data');
+        this.inputQuantidade =  $('#quantidade');
+        this.inputValor = $('#valor');
+		
+	}
+
 	adiciona(event){
 		//isso cancela a atualização da pagina na hora de submeter
 		event.preventDefault();
 
-		//estamos informando que o querySelector irá para a variável $, mas ainda manterá uma associação com document
-		let $ = document.querySelector.bind(document);
-		//Se declaramos as variáveis usando o let, estas ganharam um escopo de bloco	
-        let inputData = $('#data');
-        let inputQuantidade =  $('#quantidade');
-        let inputValor = $('#valor');
-
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
+        console.log(this.inputData.value);
+        console.log(this.inputQuantidade.value);
+        console.log(this.inputValor.value);
 
 	}
 }
