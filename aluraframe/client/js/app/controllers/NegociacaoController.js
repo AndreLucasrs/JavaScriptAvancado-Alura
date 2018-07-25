ba316a74cf86f067ad4d07ef4dbb0ded6d84ffe7
AndreLucasrs <andrelrs.v@outlook.com>
2018-07-25 00:49:30 -0300

    Curso JavaScript Avançado II: ES6, orientação a objetos e padrões de projetos, Iniciando, Aula 1

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,11 +7,21 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade =  $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        //this tem contexto dinamico, ira variar de acordo com o que esta chamando
+        //essa função abaixo tem escopo dinamico logo o this acontece o que ta escrito acima
+        //para esse this funciona de acordo com o contexto de NegociacaoController
+        //eu adicionei este parametro antes this, para mostrar qual é o contexto
+        //this._listaNegociacoes = new ListaNegociacoes(this, function(model) {
+		//mas tem como resolver com arrow function
+		// Isto ocorre porque a arrow function não é apenas uma maneira sucinta de escrever uma função,
+		//ela também tem um característica peculiar: o escopo de this é léxico,
+		//em vez de ser dinâmico como a outra função. 
+		//Isto significa que o this não mudará de acordo com o contexto
+        this._listaNegociacoes = new ListaNegociacoes((model) =>
+         	this._negociacoesView.update(model));
 
         this._negociacoesView = new NegociacoesView($("#negociacoesView"));
         this._negociacoesView.update(this._listaNegociacoes);
-
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($("#mensagemView"));
         this._mensagemView.update(this._mensagem);
@@ -23,12 +33,19 @@ class NegociacaoController{
 		event.preventDefault();
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = "Negociação adicionada com sucesso";
         this._mensagemView.update(this._mensagem);
 
         this._limpaFormulario();
+	}
+
+	apaga(){
+
+		  this._listaNegociacoes.esvazia();
+
+		  this._mensagem.texto = 'Negociações apagadas com sucesso';
+		  this._mensagemView.update(this._mensagem);
 	}
 
 	_criaNegociacao(){
--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,13 +1,22 @@
 class ListaNegociacoes {
 
-	constructor() {
+	//constructor(contexto, armadilha) {
+	constructor(armadilha) {	
 
         this._negociacoes = [];
+        //isso ira fazer a tela se atualizar sempre, pq o parametro passado é uma função anonima de update
+        //essa armadilha sempre sera chamado no adiciona ou no esvazia
+        this._armadilha = armadilha;
+        //this._contexto = contexto;
     }
 
     adiciona(negociacao) {
 
         this._negociacoes.push(negociacao);
+        //o model é a instancia de listaNegociacoes esta passando quando esta sendo chamada
+        //esse this dentro da armadilha esta recebendo a propria estancia de quem esta chamando-a
+        this._armadilha(this);
+        //Reflect.apply(this._armadilha,this._contexto,[this]);
     }
 
 	get negociacoes() {
@@ -16,6 +25,13 @@ class ListaNegociacoes {
 		//isso estar retornando a copia do array, se vc der um push agora, vc estara inserindo valores
 		//na copia da lista, e não na lista original
     	return [].concat(this._negociacoes);
+	}
+
+	esvazia(){
+
+		 this._negociacoes =[];
+		 this._armadilha(this);
+		 //Reflect.apply(this._armadilha,this._contexto,[this])
 	}
 
 }
