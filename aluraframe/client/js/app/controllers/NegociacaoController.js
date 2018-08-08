04777619197a2f8537ffd9dc60036bf3cfc1d055
AndreLucasrs <andrelrs.v@outlook.com>
2018-08-08 01:17:43 -0300

    Aula 3

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -20,45 +20,11 @@ class NegociacaoController{
         //this._listaNegociacoes = new ListaNegociacoes((model) =>
         // 	this._negociacoesView.update(model));
 
-         //O proxy é um objeto que ira ser a copia de negociacao, ele irar encapsular o objeto real
-        //e com ele a gente pode alterar codigo de infra sem problema
-        //proxy se baseia em um padrão de projeto
-        let self = this;
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(),{
+        this._listaNegociacoes = new Bind(new ListaNegociacoes(),
+        	new NegociacoesView($("#negociacoesView")),'adiciona','esvazia');
 
-//O target é o objeto real, que é encapsulado pelo proxy.
-//O prop é a propriedade que está sendo lida.
-//O receiver é uma referência ao próprio proxy.
-            get(target,prop,receiver){
-                
-                //ele verifica se dentro do vetor tem um prop, o includes, esta sendo isso
-                //quando chamado o get ele ira olhar se é um metodo ou uma funcao
-                //se for uma funcao ele
-                //ele cria uma nova função temporada no proxy
-                // e o arguments esta pegando todos os paramentros da chamada que esta fazendo para //cair nesse if
-                //nesse caso a linha abaixo lista.adiciona()
-                //então o arguments passa esse os parametros para nova função criada 
-                //se for um metodo ele retorna o get normal
-                if(['adiciona','esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)){
-
-                    return function(){
-
-                        console.log(`interceptando ${prop}`);
-                        Reflect.apply(target[prop],target,arguments);
-                        self._negociacoesView.update(target);
-                    }
-                }
-
-                return Reflect.get(target,prop,receiver);
-            }
-
-        });
-
-        this._negociacoesView = new NegociacoesView($("#negociacoesView"));
-        this._negociacoesView.update(this._listaNegociacoes);
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($("#mensagemView"));
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(new Mensagem(),
+        	new MensagemView($("#mensagemView")),'texto');
 
 	}
 
@@ -69,7 +35,6 @@ class NegociacaoController{
         this._listaNegociacoes.adiciona(this._criaNegociacao());
 
         this._mensagem.texto = "Negociação adicionada com sucesso";
-        this._mensagemView.update(this._mensagem);
 
         this._limpaFormulario();
 	}
@@ -79,7 +44,6 @@ class NegociacaoController{
 		  this._listaNegociacoes.esvazia();
 
 		  this._mensagem.texto = 'Negociações apagadas com sucesso';
-		  this._mensagemView.update(this._mensagem);
 	}
 
 	_criaNegociacao(){
---
+++ aluraframe/client/js/app/helpers/Bind.js
@@ -0,0 +1,17 @@
+class Bind {
+
+	//REST operator ...props, esses ... é o rest operator
+	//Para gerarmos um array com n parâmetros, devemos usar o REST apenas no último, outro vantajoso recurso do ECMAScript
+	// primeiro parâmetro recebido pelo Bind é o model, o segundo é a view, e a partir do terceiro, 
+	//eles caem dentro do ...props - podendo ser diversos, como um array.
+	// No nosso caso, ...props é um array com duas posições (adiciona e esvazia). 
+	//É isso que o create() do ProxyFactory espera receber. 
+	//Com a pequena adição do REST. Isto também nos permite fazer uma associação com apenas um parâmetro, 
+	//como no caso do texto de _mensagem, sem colocá-lo em um array.
+	constructor(model,view,...props){
+		let proxy = ProxyFactory.create(model, props, model =>view.update(model));
+		view.update(model);
+        	
+        return proxy;	
+	}
+}
